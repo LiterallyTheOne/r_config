483ea4afec833ef3562e29bdd1382a1d6d85057e
Ramin <mystery.ramin@gmail.com>
2021-09-29 21:09:33 +0330

    adding version 0.1.0

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='r_config_loader',
+    version="0.1.0",
     packages=['r_config_loader'],
     author="Ramin Zarebidoky",
     author_email="mystery.ramin@gmail.com",
     description="using easydict and Yaml to make loading and using config easier",
-    url="https://github.com/rbgirshick/yacs",
+    url="https://github.com/literallynomana/r_config_loader",
     long_description="using easydict and PyYaml to make loading config easier",
     install_requires=['pyyaml', 'easydict']
 )
