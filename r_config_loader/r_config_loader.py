c9c33b3bf0c455413e175590fc9e1b2d5ccd6631
Ramin <mystery.ramin@gmail.com>
2021-10-03 12:41:05 +0330

    changing the strategy of reading files, adding string based yaml to easydict

--- r_config_loader/r_config_loader.py
+++ r_config_loader/r_config_loader.py
@@ -3,24 +3,57 @@ from easydict import EasyDict
 from pathlib import Path
 
 
-def load_config(config_path):
-    # type: (str | Path) -> EasyDict
+def transfer_str_yaml_to_easydict(str_yaml):
+    # type: (str) -> EasyDict
     """
-    loads a yaml based config file
+    transfers yaml based string to EasyDict
+    :param str_yaml: yaml based string
+    :return: r_config
     """
-    with open(config_path) as config_file:
-        f_config = yaml.load(config_file, Loader=yaml.SafeLoader)
+    f_config = yaml.load(str_yaml, Loader=yaml.SafeLoader)
+
     return EasyDict(f_config)
 
 
-def update_the_main_config(config_path, r_config=None):
-    # type: (str | Path, EasyDict) -> EasyDict
+def update_the_main_config(str_yaml, r_config=None):
+    # type: (str, EasyDict) -> EasyDict
     """
-    updates r_config with the path given
+    updates r_config a with given EasyDict
+    :param str_yaml: yaml based string
+    :param r_config: previous r_config
+    :return: updated r_config
     """
     if r_config is None:
         r_config = EasyDict()
 
-    cf = load_config(config_path)
+    cf = transfer_str_yaml_to_easydict(str_yaml)
+
     r_config.update(cf)
+    return r_config
+
+
+def load_config(config_path):
+    # type: (str | Path) -> str
+    """
+    loads a yaml based config file as string
+    :param config_path: path of config file
+    :return: content of config file
+    """
+    with open(config_path) as config_file:
+        result = config_file.read()
+    return result
+
+
+def update_the_main_config_with_path(config_path, r_config=None):
+    # type: (str | Path, EasyDict) -> EasyDict
+    """
+    updates r_config with a given path
+    :param config_path: path of config file
+    :param r_config: previous r_config
+    :return: updated r_config
+    """
+    str_yaml = load_config(config_path)
+
+    r_config = update_the_main_config(str_yaml, r_config)
+
     return r_config
