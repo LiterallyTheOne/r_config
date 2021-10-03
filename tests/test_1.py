29d98f6fb1809db41a148b4da7425570c3db6895
Ramin <mystery.ramin@gmail.com>
2021-10-03 12:41:59 +0330

    adding test to update_the_main_config

---
+++ tests/test_1.py
@@ -0,0 +1,32 @@
+import pytest
+from easydict import EasyDict
+
+from r_config_loader.r_config_loader import update_the_main_config
+
+
+@pytest.fixture()
+def r_config():
+    # type: () -> EasyDict
+    return EasyDict()
+
+
+@pytest.fixture()
+def yaml_str():
+    # type: () -> str
+
+    return """
+    yek:
+      ramin: 2
+      mehran: 'salam'
+    # comment to test
+    dow:
+      se:
+        p1: 2.2
+      f1: 'khodafez'
+    """
+
+
+def test_update_the_main_config(r_config, yaml_str):
+    update_the_main_config(yaml_str, r_config)
+
+    assert hasattr(r_config, 'yek')
