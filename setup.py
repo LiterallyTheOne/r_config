b8a42e1b8886db33d40d89ef0ace5b523e868092
Ramin Zarebidoky <mystery.ramin@gmail.com>
2021-10-04 09:21:41 +0330

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='r_config_loader',
-    version="0.1.1",
+    version="0.2.0",
     packages=['r_config_loader'],
     author="Ramin Zarebidoky",
     author_email="mystery.ramin@gmail.com",
