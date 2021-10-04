0f7f30bc34697be6c2019d43473198a63a9916f3
Ramin Zarebidoky <mystery.ramin@gmail.com>
2021-10-04 09:01:56 +0330

    correcting the test

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,6 +1,6 @@
 import pytest
 
-from r_config_loader.r_config_loader import RConfig, update_the_main_config
+from r_config_loader.r_config_loader import RConfig
 
 
 @pytest.fixture()
@@ -26,6 +26,6 @@ def yaml_str():
 
 
 def test_update_the_main_config(r_config, yaml_str):
-    update_the_main_config(yaml_str, r_config)
+    r_config.update_from_str(yaml_str)
 
     assert hasattr(r_config, 'yek')
