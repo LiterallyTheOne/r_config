7d52b1dba356e9669b56e339c88d728820e02588
Ramin Zarebidoky <mystery.ramin@gmail.com>
2021-10-04 09:00:58 +0330

    adding methods to class

--- r_config_loader/r_config_loader.py
+++ r_config_loader/r_config_loader.py
@@ -8,58 +8,49 @@ class RConfig(EasyDict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def update_from_file(self, config_path):
+        # type: (str | Path) -> None
+        """
+        updates r_config with a given path
+        :param config_path:
+        :return: None
+        """
+        str_yaml = RConfig._load_config(config_path)
 
-def transfer_str_yaml_to_easydict(str_yaml):
-    # type: (str) -> RConfig
-    """
-    transfers yaml based string to RConfig
-    :param str_yaml: yaml based string
-    :return: r_config
-    """
-    f_config = yaml.load(str_yaml, Loader=yaml.SafeLoader)
+        self.update_from_str(str_yaml)
 
-    return RConfig(f_config)
+    def update_from_str(self, str_yaml):
+        # type: (str) -> None
+        """
+        updates r_config a with given RConfig
+        :param str_yaml: yaml based string
+        :return: None
+        """
 
+        cf = RConfig._transfer_str_yaml_to_rconfig(str_yaml)
 
-def update_the_main_config(str_yaml, r_config=None):
-    # type: (str, RConfig) -> RConfig
-    """
-    updates r_config a with given RConfig
-    :param str_yaml: yaml based string
-    :param r_config: previous r_config
-    :return: updated r_config
-    """
-    if r_config is None:
-        r_config = RConfig()
+        self.update(cf)
 
-    cf = transfer_str_yaml_to_easydict(str_yaml)
+    @staticmethod
+    def _load_config(config_path):
+        # type: (str | Path) -> str
+        """
+        loads a yaml based config file as string
+        :param config_path: path of config file
+        :return: content of config file
+        """
+        with open(config_path) as config_file:
+            result = config_file.read()
+        return result
 
-    r_config.update(cf)
-    return r_config
+    @staticmethod
+    def _transfer_str_yaml_to_rconfig(str_yaml):
+        # type: (str) -> RConfig
+        """
+        transfers yaml based string to RConfig
+        :param str_yaml: yaml based string
+        :return: r_config
+        """
+        f_config = yaml.load(str_yaml, Loader=yaml.SafeLoader)
 
-
-def load_config(config_path):
-    # type: (str | Path) -> str
-    """
-    loads a yaml based config file as string
-    :param config_path: path of config file
-    :return: content of config file
-    """
-    with open(config_path) as config_file:
-        result = config_file.read()
-    return result
-
-
-def update_the_main_config_with_path(config_path, r_config=None):
-    # type: (str | Path, RConfig) -> RConfig
-    """
-    updates r_config with a given path
-    :param config_path: path of config file
-    :param r_config: previous r_config
-    :return: updated r_config
-    """
-    str_yaml = load_config(config_path)
-
-    r_config = update_the_main_config(str_yaml, r_config)
-
-    return r_config
+        return RConfig(f_config)
