225dfe8e8e13cfc1d8aba4968f93335d0d816ee8
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2021-10-05 10:33:36 +0330

    changing version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='r_config_loader',
-    version="0.2.0",
+    version="0.2.1",
     packages=['r_config_loader'],
     author="Ramin Zarebidoky",
     author_email="ramin.zarebidoky@gmail.com",
