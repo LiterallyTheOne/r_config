e33c5b1cb19cdcf09a13ff8cf18ac841d5ae7f27
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2021-10-08 13:42:20 +0330

    updating url

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Ramin Zarebidoky",
     author_email="ramin.zarebidoky@gmail.com",
     description="Standard way that I am using to use config",
-    url="https://github.com/literallynomana/r_config",
+    url="https://github.com/RaminZarebidoky/r_config",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['pyyaml']
