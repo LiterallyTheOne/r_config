881b143007191f506e834d5e425aeb43d7feab27
Ramin Zarebidoky <Ramin.zarebidoky@gmail.com>
2024-06-06 09:24:45 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='r_config',
-    version="0.4.1",
+    version="0.4.2",
     packages=['r_config'],
     author="Ramin Zarebidoky",
     author_email="ramin.zarebidoky@gmail.com",
